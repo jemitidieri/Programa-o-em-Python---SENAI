0dc5d15c486507c5e44f593853357289783741ca
jemitidieri <je_mitidieri@yahoo.com.br>
2025-10-23 21:55:53 -0300

    Add files via upload

---
+++ Aula_3 - Dados Primitivos e Funções/aula3.py
@@ -0,0 +1,48 @@
+# # Trabalhando com dados primitivos
+# # dado literal
+
+# print('Texto', 4, 5.8, True)
+
+# print(10+10)
+# print(10-10)
+# print(10*10)
+# print(10/10)
+
+# print(10//10)
+# print(10%10)
+
+# # sinais logicos
+
+# print(10>10)
+# print(10<10)
+# print(10<=10)
+# print(10>=10)
+# print(10!=10)
+# print(10==10)
+
+
+
+# variaveis - estrutura de dados
+
+# variaveis, listas, tuplas e dicionarios
+# 1 dado
+
+# n1= int(input('digite um numero '))
+# n2= int(input('digite outro numero '))
+# soma = n1 + n2
+# print(soma)
+
+# n3= int(input('digite um numero '))
+# n4= int(input('digite outro numero '))
+# sub = n3 - n4
+# print(sub)
+
+# n5= int(input('digite um numero '))
+# n6= int(input('digite outro numero '))
+# mult = n5 * n6
+# print(mult)
+
+n7= int(input('digite um numero '))
+n8= int(input('digite outro numero '))
+div = n7 // n8
+print(div)
