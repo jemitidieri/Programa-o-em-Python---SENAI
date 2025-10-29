559a95008fc26cd30dbb349a6d8ce3b97343faa2
jemitidieri <je_mitidieri@yahoo.com.br>
2025-10-28 21:28:34 -0300

    Add files via upload

---
+++ Aula 5/Aula5.py
@@ -0,0 +1,67 @@
+# # UTILIZE VARIÁVEIS!!!!!! 
+
+
+# # Utilize as 5 concatenações, teste todas 
+
+# # 1 Imprima uma mensagem de boas-vindas na tela.
+# print('Bom dia!')
+
+# # 2 Declare uma variável booleana verdadeiro
+# # com o valor True e imprima seu tipo
+# x = True
+# print(type(x))
+
+# # 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+# n1 = 5.2
+# n2 = 5.8
+
+# print(n1 * n2)
+
+# # 4 Imprima o resultado da divisão (/)de dois números inteiros 
+# # de sua escolha.
+# n1 = 10
+# n2 = 2
+# print(n1 / n2)
+
+# # 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+# n1 = 10
+# n2 = 2
+# print(n1 - n2)
+
+# # 6 Imprima o resultado da divisão (//)inteira de dois números inteiros 
+# # de sua escolha.
+# n1 = 10
+# n2 = 2
+# print(n1 // n2)
+# # 7 Imprima o resultado da multiplicação de 4 números 
+# # decimais de sua escolha
+# n1 = 10
+# n2 = 3
+# n3 = 6
+# n4 = 10
+# print(n1 * n2 * n3 * n4)
+
+
+# # 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# # imprima o dobro desse número
+# var  =  10
+# print(var ** 2)
+
+# 9 Crie um sistema de cadastro com as estruturas que vc já 
+# conhece(Use apenas input, print e variavel)
+
+# nome, email, idade, cidade, graduação, estado civil
+nome = input('Digite seu nome: ')
+
+email = input('Digite seu e-mail: ')
+
+idade = input('Qual a sua idade? ')
+
+cidade = input('Qual sua cidade? ')
+
+graduação = input('Qual o curso da sua graduação? ')
+
+estado_civil = input('Qual seu estado civil? ')
+
+print(f'{nome}\n{email}\n{idade}\n{cidade}\n{graduação}\n{estado_civil}')
+
