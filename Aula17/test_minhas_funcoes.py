30efd90f933248e75286b2d27ba2766ae689af16
jemitidieri <je_mitidieri@yahoo.com.br>
2025-11-14 21:50:18 -0300

    Add files via upload

---
+++ Aula17/test_minhas_funcoes.py
@@ -0,0 +1,6 @@
+import minhas_funcoes
+
+def test_somar():
+       assert minhas_funcoes.somar(2, 3) == 5
+       assert minhas_funcoes.somar(5, 7) == 12
+       assert minhas_funcoes.somar(10, -3) == 7
